599f5d8005ec2cfba8f49c3c2aac340dbe35a859
Andrii Litvin <al@gogmat.com>
2018-06-07 14:16:47 +0300

    fix

--- src/js/component.js
+++ src/js/component.js
@@ -249,9 +249,11 @@ $(document).ready(function () {
 
   $('.play-btn').click(function () {
     var iframe_url = "https://www.youtube.com/embed/" + $(this).parent().data('id') + "?autoplay=1&autohide=1&rel=0&amp;showinfo=0";
+    var iframe_height = $(this).parent().find('.preview').height();
+    var iframe_width = $(this).parent().find('.preview').width();
     $(this).hide();
     $(this).parent().find('.preview').hide();
-    $(this).parent().append('<iframe src="' + iframe_url + '?rel=0&amp;controls=0&amp;showinfo=0"  width="' + $(this).parent().find('.preview').width() + '" height="' + $(this).parent().find('.preview').height() + '"  frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
+    $(this).parent().append('<iframe src="' + iframe_url + '?rel=0&amp;controls=0&amp;showinfo=0"  width="'+ iframe_width + '" height="' + iframe_height + '"  frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
   });
 
   $('.scroll').click(function (e) {
